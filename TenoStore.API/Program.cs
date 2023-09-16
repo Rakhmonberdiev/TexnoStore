d1c47c97f98a8f6a79d0e6182c19e367a809d7fc
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-16 21:11:01 +0500

    Adding a Repository and Interface

--- TenoStore.API/Program.cs
+++ TenoStore.API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using System;
+using TexnoStore.Core.Interfaces;
 using TexnoStore.Infrastructure.Data;
+using TexnoStore.Infrastructure.Data.Implementation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,7 @@ builder.Services.AddDbContext<TexnoStoreContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ TexnoStore.Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TexnoStore.Core.Entities;
+
+namespace TexnoStore.Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task <Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+    }
+}
