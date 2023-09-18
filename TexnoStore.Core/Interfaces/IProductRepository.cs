3896f3386e907e58882ef06978767cd5cab31320
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-18 17:06:06 +0500

    Adding the code to get the product brands and types

--- TexnoStore.Core/Interfaces/IProductRepository.cs
+++ TexnoStore.Core/Interfaces/IProductRepository.cs
@@ -11,5 +11,7 @@ namespace TexnoStore.Core.Interfaces
     {
         Task <Product> GetProductByIdAsync(int id);
         Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductTypesAsync();
     }
 }
