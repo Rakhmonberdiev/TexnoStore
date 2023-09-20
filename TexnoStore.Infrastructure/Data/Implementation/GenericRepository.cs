812adab9e6dac9ec53de24602b8a2a622778e79d
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-20 11:36:40 +0500

    Implementing the repository with specification methods

--- TexnoStore.Infrastructure/Data/Implementation/GenericRepository.cs
+++ TexnoStore.Infrastructure/Data/Implementation/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TexnoStore.Core.Entities;
 using TexnoStore.Core.Interfaces;
+using TexnoStore.Core.Specifications;
 
 namespace TexnoStore.Infrastructure.Data.Implementation
 {
@@ -16,9 +17,25 @@ namespace TexnoStore.Infrastructure.Data.Implementation
            return await _context.Set<T>().FindAsync(id);
         }
 
+
         public async Task<IReadOnlyList<T>> ListAllAsync()
         {
             return await _context.Set<T>().ToListAsync();
+        }
+        public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+        {
+            return await ApplySpec(spec).FirstOrDefaultAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpec(spec).ToListAsync();
+        }
+
+
+        private IQueryable<T> ApplySpec(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
     }
 }
