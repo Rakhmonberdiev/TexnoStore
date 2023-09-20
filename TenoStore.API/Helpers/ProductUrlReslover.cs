41bacee5d8f26dafe0554f95c1f166dc938eb0b1
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-20 18:14:57 +0500

    Adding a Custom Value Resolver for AutoMapper

---
+++ TenoStore.API/Helpers/ProductUrlReslover.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using TenoStore.API.Dtos;
+using TexnoStore.Core.Entities;
+
+namespace TenoStore.API.Helpers
+{
+    public class ProductUrlReslover : IValueResolver<Product, ProductToReturnDto, string>
+    {
+        private readonly IConfiguration _configuration;
+        public ProductUrlReslover(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+           if(!string.IsNullOrEmpty(source.PictureUrl)) 
+           {
+                return _configuration["ApiUrl"] + source.PictureUrl;    
+           }
+            return null;
+        }
+    }
+}
--- TenoStore.API/Program.cs
+++ TenoStore.API/Program.cs
@@ -30,7 +30,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
