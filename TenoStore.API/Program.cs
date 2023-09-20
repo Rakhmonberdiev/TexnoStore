a23133da802a7c2551392143fb133b4ec5d73818
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-20 20:59:22 +0500

    Cleaning up the Program class

--- TenoStore.API/Program.cs
+++ TenoStore.API/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using TenoStore.API.Errors;
+using TenoStore.API.Extensions;
 using TenoStore.API.Helpers;
 using TenoStore.API.Middleware;
 using TexnoStore.Core.Interfaces;
@@ -11,48 +12,22 @@ using TexnoStore.Infrastructure.Data.Implementation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TexnoStoreContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddSwaggerDoc();
+
+
 builder.Services.AddControllers();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(typeof(MappingProfiles));
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = actionContext =>
-    {
-        var errors = actionContext.ModelState
-        .Where(e => e.Value.Errors.Count > 0)
-        .SelectMany(x => x.Value.Errors)
-        .Select(x => x.ErrorMessage).ToArray();
-
-        var errorResponse = new ApiValidationErrorResponse
-        {
-            Errors = errors,
-
-        };
-        return new BadRequestObjectResult(errorResponse);
-    };
-});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwaggerDoc();
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthorization();
-app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
