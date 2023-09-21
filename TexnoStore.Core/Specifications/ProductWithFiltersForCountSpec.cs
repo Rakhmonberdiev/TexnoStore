7daf7107cf9475f0366b44574258746a4e8828ef
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-21 17:00:17 +0500

    Adding the search functionality

--- TexnoStore.Core/Specifications/ProductWithFiltersForCountSpec.cs
+++ TexnoStore.Core/Specifications/ProductWithFiltersForCountSpec.cs
@@ -11,6 +11,7 @@ namespace TexnoStore.Core.Specifications
     {
         public ProductWithFiltersForCountSpec(ProductSpecParams productParams)
                         : base(x =>
+                 (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
                  (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
                  (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
             )
