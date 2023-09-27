9fbbd65f9f5ee0da8c0ab369f41d8ec1b220f9cb
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-27 09:50:21 +0500

    Implementing the basket repository

--- TexnoStore.Infrastructure/Data/Implementation/BasketRepository.cs
+++ TexnoStore.Infrastructure/Data/Implementation/BasketRepository.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using StackExchange.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using TexnoStore.Core.Entities;
 using TexnoStore.Core.Interfaces;
@@ -10,19 +12,33 @@ namespace TexnoStore.Infrastructure.Data.Implementation
 {
     public class BasketRepository : IBasketRepository
     {
-        public Task<bool> DeleteBasketAsync(string basketId)
+        private readonly IDatabase _database;
+        public BasketRepository(IConnectionMultiplexer redis)
         {
-            throw new NotImplementedException();
+            _database = redis.GetDatabase();
+            
+        }
+        public async Task<bool> DeleteBasketAsync(string basketId)
+        {
+            return await _database.KeyDeleteAsync(basketId);
         }
 
-        public Task<CustomerBasket> GetBasketAsync(string basketId)
+        public async Task<CustomerBasket> GetBasketAsync(string basketId)
         {
-            throw new NotImplementedException();
+           var data = await _database.StringGetAsync(basketId);
+           return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(data);
         }
 
-        public Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
         {
-            throw new NotImplementedException();
+            var created = await _database.StringSetAsync(basket.Id,
+                JsonSerializer.Serialize(basket),
+                TimeSpan.FromDays(30));
+            if (!created)
+            {
+                return null;
+            }
+            return await GetBasketAsync(basket.Id);
         }
     }
 }
