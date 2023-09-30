df9ef92ea7bf0f0f5bd894a20c3d5409a9cbbcc7
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-09-30 20:42:57 +0500

    Adding an Account controller

---
+++ TenoStore.API/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TenoStore.API.Dtos;
+using TenoStore.API.Errors;
+using TexnoStore.Core.Entities;
+
+namespace TenoStore.API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+
+            if (user == null) return Unauthorized(new ApiResponse(401));
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized(new ApiResponse(401));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                Token = "token",
+                DisplayName = user.DisplayName
+            };
+        }
+    }
+}
