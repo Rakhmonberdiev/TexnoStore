39f29aa8d641052493702dc1cbfb19cc8df86740
Jamoliddin <j.m.raxmonberdiyev@gmail.com>
2023-10-02 16:38:43 +0500

    token generation service

---
+++ TexnoStore.Core/Interfaces/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TexnoStore.Core.Entities;
+
+namespace TexnoStore.Core.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user);
+    }
+}
